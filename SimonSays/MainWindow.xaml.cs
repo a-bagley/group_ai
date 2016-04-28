bd9073f7bcd8cfaf914eddd212e4294a7362077c
SamBird <sam.birdd@hotmail.co.uk>
2016-04-28 15:55:17 +0100

    Added RadioButton functionality to change the AI technique to execute

--- SimonSays/MainWindow.xaml.cs
+++ SimonSays/MainWindow.xaml.cs
@@ -3,33 +3,37 @@ using SimonSays.Utils;
 
 namespace SimonSays
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AISystemEnum aiTechniqueEnum;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.rbANN.IsChecked = true;
         }
 
         private void btnEasy_Click(object sender, RoutedEventArgs e)
         {
-            var window = new GameWindow(AISystemEnum.NN, DifficultyEnum.Easy);
+            var window = new GameWindow(aiTechniqueEnum, DifficultyEnum.Easy);
             window.Show();
             this.Close();
         }
 
         private void btnMedium_Click(object sender, RoutedEventArgs e)
         {
-            var window = new GameWindow(AISystemEnum.NN, DifficultyEnum.Medium);
+            var window = new GameWindow(aiTechniqueEnum, DifficultyEnum.Medium);
             window.Show();
             this.Close();
         }
 
         private void btnHard_Click(object sender, RoutedEventArgs e)
         {
-            var window = new GameWindow(AISystemEnum.NN, DifficultyEnum.Hard);
+            var window = new GameWindow(aiTechniqueEnum, DifficultyEnum.Hard);
             window.Show();
             this.Close();
         }
@@ -39,6 +43,18 @@ namespace SimonSays
             var window = new TrainWindow();
             window.Show();
             this.Close();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.rbANN.IsChecked == true)
+            {
+                aiTechniqueEnum = AISystemEnum.NN;
+            } 
+            else if (this.rbNB.IsChecked == true) 
+            {
+                aiTechniqueEnum = AISystemEnum.NaiveBayes;
+            }
         }
     }
 }
